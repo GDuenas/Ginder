7fdddfb3a53973cf5ffb84a8e643ffea65e57b75
Gabe <gabe9462@gmail.com>
2022-03-04 18:51:20 -0500

    add simulation wip

---
+++ simulation.js
@@ -0,0 +1,13 @@
+var Simlulator = (function() {
+
+
+
+	entity.simulate = function() {
+		var firstname = document.getElementById("firstname");
+		var lastname = document.getElementById("lastname");
+		var email = document.getElementById("email");
+		var gamertag = document.getElementById("gamertag");
+		var gameconsole = document.getElementById("console");
+	}
+
+}());
