13bbd4c920612d2bf9724f504281b9adf8ab54b4
Gabe <gabe9462@gmail.com>
2022-03-04 19:19:25 -0500

    add simulation file

--- simulation.js
+++ simulation.js
@@ -1,13 +1,25 @@
 var Simlulator = (function() {
 
-
-
 	entity.simulate = function() {
 		var firstname = document.getElementById("firstname");
 		var lastname = document.getElementById("lastname");
 		var email = document.getElementById("email");
 		var gamertag = document.getElementById("gamertag");
 		var gameconsole = document.getElementById("console");
+
+		var submitbutton = document.getElementById("submit-info");
+
+
+		firstname.value = "Joe";
+		lastname.value = "Mama";
+		email.value = "joe@mama.ca";
+		gamertag.value = "mamaphone";
+		gameconsole.value = "pc";
+
+		submitbutton.click();
+
 	}
+
+
 
 }());
